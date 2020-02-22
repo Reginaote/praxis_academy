d3bb33dded2eba2ec9c9c1ed904d50635ddb6243
Reginaote <43957413+Reginaote@users.noreply.github.com>
2020-02-22 18:17:55 +0700

    Delete Get-LastBootTime1.ps1

--- kemampuan-dasar/kemampuan-dasar-1/Get-LastBootTime1.ps1
+++
@@ -1,7 +0,0 @@
-﻿param(
-[string]$ComputerName
-)
-
-Get-WmiObject -Class Win32_OperatingSystem –ComputerName $ComputerName |
-Select-Object -Property CSName,@{n=”Last Booted”;
-e={[Management.ManagementDateTimeConverter]::ToDateTime($_.LastBootUpTime)}}
